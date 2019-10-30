b163889a0acb383896cfa5d9457f35224da4efc3
tvladi <buss15t@yandex.ru>
2019-10-30 10:13:57 +0300

    lesson 1.7

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
    public static void main(String[] args) {
-      System.out.println("Hello world");
+      hello("Tatiana");
+
+      double len = 3;
+      System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+      double a = 5;
+      double b = 6;
+      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
    }
-   
+
+   public static void hello (String user) {
+      System.out.println("Hello " + user);
+   }
+
+   public static double area (double l) {
+      return l * l;
+   }
+
+   public static double area (double a, double b) {
+      return a * b;
+   }
 }
